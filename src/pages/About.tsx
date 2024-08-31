3bc120dcd638693bcc484fc34edd185f2a4fde3a
Tirth Patel <t83patel@uwaterloo.ca>
2024-08-31 13:15:23 -0400

    added small fix to about page'

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -23,7 +23,7 @@ class About extends React.Component<{}, AboutState> {
   }
   render() {
     const one = (
-      <h1 style={{paddingTop:'15px'}}>Enjoy finding solutions to problems and creating meaningful tech.</h1>
+      <h1 style={{paddingTop:'15px'}}>Enjoy finding solutions to complex problems and creating meaningful tech.</h1>
     );
     const two = (
       <h2>
